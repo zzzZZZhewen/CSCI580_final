ecc66efc15e330e1117a6646aa07d9268f7c5be1
Miao <zhewenqq@live.com>
2018-11-12 18:52:07 -0800

    Wrapped application.

--- final/Application.h
+++ final/Application.h
@@ -1,4 +1,65 @@
 #pragma once
 
+#include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+class Application
+{
+public:
+	Application();
+	~Application();	
+	int Run();
+private:
+	GLFWwindow *window;
+	int InitGL();
+	unsigned int VBOs[2], VAOs[2];
+	unsigned int shaderProgramOrange;
+	unsigned int shaderProgramYellow;
+};
+
+
+static const char *vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+static const char *fragmentShader1Source = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n\0";
+static const char *fragmentShader2Source = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);\n"
+"}\n\0";
+
+static const unsigned int SCR_WIDTH = 800;
+static const unsigned int SCR_HEIGHT = 600;
+
+static void error_callback(int error, const char* description)
+{
+	fputs(description, stderr);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+static void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// make sure the viewport matches the new window dimensions; note that width and 
+	// height will be significantly larger than specified on retina displays.
+	glViewport(0, 0, width, height);
+}
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+static void processInput(GLFWwindow *window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
+}
